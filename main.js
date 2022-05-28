718ed86bfdf33457d4869cd6c605362d870457a2
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-05-28 10:05:23 -0600

    Tipos de datos | Number

--- main.js
+++ main.js
@@ -8,13 +8,17 @@ asdf
 asdf
 */
 
-var nombre = "Pablo";
-var edad = 15;
+var nombre = "Pedro";
+var edad = 50;
 const fechaNacMaria = "15/08/1990"
 
 console.log(nombre);
 console.log(edad);
 console.log(nombre);
+
+console.log(
+    nombre + " tiene " + edad + " años " + "porque nació el " + fechaNacMaria
+)
 
 // String (cadenas)
 var cadena = "Cadena 'Blablabla'";
@@ -26,3 +30,23 @@ console.log(cadena)
 console.log(cadena2)
 console.log(cadena3)
 console.log(cadena4)
+
+//Number
+var numero = 1;
+var numero2 = 0.01;
+var numero3 = -0.01;
+var numero4 = 1.5e+6;
+
+console.log(numero)
+console.log(numero2)
+console.log(numero3)
+console.log(numero4)
+
+var infinito = 12313123112312/0;
+var otra = 15 + +"Cadena";
+
+console.log(infinito)
+console.log(typeof infinito)
+console.log(otra)
+console.log(typeof otra)
+
